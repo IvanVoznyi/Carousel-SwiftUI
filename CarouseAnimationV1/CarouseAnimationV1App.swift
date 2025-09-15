e750ca2ea4ed8e5bf26fafbd646a2abcc7248469
Ivan Voznyi <ivan.voznyi92@gmail.com>
2025-09-15 11:25:52 +0300

    Initial Commit

---
+++ CarouseAnimationV1/CarouseAnimationV1App.swift
@@ -0,0 +1,17 @@
+//
+//  CarouseAnimationV1App.swift
+//  CarouseAnimationV1
+//
+//  Created by Ivan Voznyi on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CarouseAnimationV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
